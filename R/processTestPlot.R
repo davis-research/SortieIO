f82223a3e3cfde821ffae236826f282e7f0a5c77
ecology-rocks <clubtenna@gmail.com>
2015-11-03 13:52:38 -0800

    Updating documentation to make it more useful

--- R/processTestPlot.R
+++ R/processTestPlot.R
@@ -1,6 +1,27 @@
-#' Process a plot's test data
+#'Process a plot's test data
 #'
-#' @export
+#'@param plotname The name of the plot. This must be the same as what is looked
+#'  up by realPlot, or it will bounce back.
+#'@param sdir The directory location of the output files
+#'@param yearoffset If there's a run-up -- early data you want to throw out --
+#'  add it here. Default is 0.
+#'@param charactername The character name to analyze. Current options are
+#'  "SdlAbsDen", "SaplAbsDen", "AdultAbsDen", "SaplAbsBA", and "AdultAbsBA"
+#'@param writeimage Pass a filename in to allow the program to generate a plot
+#'  and write it to file.
+#'@param writefile Pass a filename in to allow the program to generate a CSV and
+#'  write it to file.
+#'@param byspecies If you want to separate the data by plot, set this to FALSE.
+#'@param numsubplots The number of subplots in the file. It defaults to 1, the
+#'  entire plot "subplot"
+#'@param subplotid The subplot you want to analyze. Default ID is 0, for the
+#'  entire plot.
+#'@param testSim A testing function, if set to true, the execution will stop and
+#'  return the simulated data instead of the full result.
+#'@param combineAdults Defaults to true, if false, Adult analyses will exclude
+#'  sapling and seedling counts and basal areas.
+#'
+#'@export
 
 processTestPlot <- function(plotname, sdir, yearoffset, charactername="",
                             writeimage=NULL, writefile=NULL, byspecies=F,
